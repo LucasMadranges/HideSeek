9e7617bdad3192d9d27da30e0999e8b722680777
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-03-09 20:35:38 +0100

    🌟 feat(router): add `/app` route and `TestPage.vue`
    Added a new route `/app` and its corresponding `TestPage.vue` component to extend the application's navigation. Also standardized code formatting across files (e.g., quotes styles) for consistency.

---
+++ front/src/pages/TestPage.vue
@@ -0,0 +1,14 @@
+<script lang="ts"
+        setup>
+
+</script>
+
+<template>
+  <q-page>
+    <h1>Hello world</h1>
+  </q-page>
+</template>
+
+<style scoped>
+
+</style>
--- front/src/router/routes.ts
+++ front/src/router/routes.ts
@@ -1,17 +1,25 @@
-import type { RouteRecordRaw } from 'vue-router';
+import type {RouteRecordRaw} from "vue-router";
 
 const routes: RouteRecordRaw[] = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      {
+        path: "",
+        component: () => import("pages/IndexPage.vue"),
+      },
+      {
+        path: "app",
+        component: () => import("pages/TestPage.vue"),
+      },
+    ],
   },
-
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
   },
 ];
 
