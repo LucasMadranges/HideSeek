c54392b537c5a6ecd0f7505de53f2d40bfa6cefa
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-03-13 21:09:33 +0100

    🎨 style(IndexPage.vue, MainLayout.vue): enhance UI elements and layout
    Improved the styling of `IndexPage` with updated typography, buttons, and responsive adjustments. Added a background image and overlay to `MainLayout` for a more polished appearance.

--- front/src/layouts/MainLayout.vue
+++ front/src/layouts/MainLayout.vue
@@ -1,11 +1,46 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout class="overflow-hidden"
+            view="">
     <q-page-container>
-      <router-view />
+      <div class="image-container">
+        <img alt="Description"
+             class="bg-img"
+             src="../assets/bg-main.png">
+      </div>
+      <router-view/>
     </q-page-container>
   </q-layout>
 </template>
 
-<script setup lang="ts">
+<script lang="ts"
+        setup>
 
 </script>
+
+<style scoped>
+.image-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+}
+
+.bg-img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.image-container::after {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 51, 0.6)
+}
+
+</style>
