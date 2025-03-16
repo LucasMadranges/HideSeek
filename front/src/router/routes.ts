f6e39c0c57ab7b4f844fe5c19c66f4fbef2616aa
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-03-16 10:33:11 +0100

    🎨 feat(InscriptionPage.vue): add registration form layout
    Added a user registration form with inputs for username, email, password, and password confirmation, including a submit button. Also updated routes to include a path for the new registration page.

--- front/src/router/routes.ts
+++ front/src/router/routes.ts
@@ -14,6 +14,10 @@ const routes: RouteRecordRaw[] = [
         component: () => import("pages/ConnexionPage.vue"),
       },
       {
+        path: "/register",
+        component: () => import("pages/InscriptionPage.vue"),
+      },
+      {
         path: "party",
         component: () => import("pages/PartyPage.vue"),
       },
