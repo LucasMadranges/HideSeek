a120ea90203d65d983f977ce4972414a4075bcb5
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-03-16 10:33:11 +0100

    🎨 feat(InscriptionPage.vue): add registration form layout
    Added a user registration form with inputs for username, email, password, and password confirmation, including a submit button. Also updated routes to include a path for the new registration page.

--- front/src/pages/InscriptionPage.vue
+++ front/src/pages/InscriptionPage.vue
@@ -1,12 +1,44 @@
+<template>
+  <q-page class="flex items-center justify-center">
+    <div class="bg-white q-pa-lg rounded-borders flex column"
+         style="gap: 1rem;">
+      <h3 class="q-mt-lg q-mb-none q-mx-xl">Inscription</h3>
+      <div class="q-my-lg"
+           style="border-bottom: 1px solid #ccc;"></div>
+      <q-input v-model="textUsername"
+               :dense="dense"
+               label="Nom d'utilisateur"
+               outlined/>
+      <q-input v-model="textEmail"
+               :dense="dense"
+               label="Email"
+               outlined/>
+      <q-input v-model="textPassword"
+               :dense="dense"
+               label="Mot de passe"
+               outlined/>
+      <q-input v-model="textConfirmPassword"
+               :dense="dense"
+               label="Confirmation de mot de passe"
+               outlined/>
+      <div class="q-my-lg"
+           style="border-bottom: 1px solid #ccc;"></div>
+      <q-btn color="blue"
+             label="S'inscrire"
+             push
+             text-color="black"/>
+    </div>
+  </q-page>
+</template>
+
 <script lang="ts"
         setup>
 
+import {ref} from "vue";
+
+const textUsername = ref("");
+const textEmail = ref("");
+const textPassword = ref("");
+const textConfirmPassword = ref("");
+const dense = ref(false);
 </script>
-
-<template>
-  <q-page></q-page>
-</template>
-
-<style scoped>
-
-</style>
