69d5997c3d9cd34c10a9af4bf55ddef1760304a2
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-03-17 14:34:04 +0100

    🎵 feat(SoundManager.ts): add sound playback functionality
    Implemented a `SoundManager` class for loading and playing random sounds. Integrated it into `TauntEvent.vue` to play themed sounds on button interaction. Added multiple sound effect assets to enhance user experience.

--- front/src/components/TauntEvent.vue
+++ front/src/components/TauntEvent.vue
@@ -13,14 +13,38 @@
         setup>
 
 import {ref} from "vue";
+import {SoundManager} from "components/SoundManager";
+
+import soundUniversalFunnyTheme from "../assets/sounds/universal_funny_theme.mp3";
+import soundEmotionalDamage from "../assets/sounds/emotional_damage.mp3";
+import soundBadToTheBone from "../assets/sounds/bad-to-the-bone-meme.mp3";
+import soundBruh from "../assets/sounds/bruh-effect.mp3";
+import soundCartoonChase from "../assets/sounds/cartoon-chase-effect.mp3";
+import soundMariosAgony from "../assets/sounds/marios-agony-effect.mp3";
+import soundNoNoSquare from "../assets/sounds/no-no-square-effect.mp3";
+import soundWidePutinWalkin from "../assets/sounds/wide-putin-walkin.mp3";
+import soundXFile from "../assets/sounds/x-files-theme.mp3";
 
 function handleClick() {
+  const soundManager = new SoundManager([
+    soundUniversalFunnyTheme,
+    soundEmotionalDamage,
+    soundBadToTheBone,
+    soundBruh,
+    soundCartoonChase,
+    soundMariosAgony,
+    soundNoNoSquare,
+    soundWidePutinWalkin,
+    soundXFile,
+  ]);
+
   const btn = document.querySelector("#event-btn") as HTMLElement;
   const tauntActive = ref(false);
 
   if (btn && !tauntActive.value) {
     btn.style.backgroundColor = "#499613";
     tauntActive.value = true;
+    soundManager.playRandom();
 
     setTimeout(() => {
       btn.style.backgroundColor = "#060b39";
