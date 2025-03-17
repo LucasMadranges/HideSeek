7310bf95491b247ee1c652a1cb3317f4abe7b59f
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-03-17 12:13:39 +0100

    🎨 refactor(TauntEvent.vue): enhance button functionality
    Replaced static div with a button and added a click handler. The button now changes background color dynamically for 5 seconds when clicked, improving interactivity and user feedback.

--- front/src/components/TauntEvent.vue
+++ front/src/components/TauntEvent.vue
@@ -1,13 +1,27 @@
-<script lang="ts"
-        setup>
-
-</script>
-
 <template>
-  <div class="q-pa-sm"
-       style="background-color: #060b39; position:absolute; bottom: 64px; right: 12px; z-index: 10; border-radius: 12px;">
+  <button id="event-btn"
+          class="q-pa-sm"
+          style="background-color: #060b39; position:absolute; bottom: 64px; right: 12px; z-index: 10; border-radius: 12px; border: none; transition: all 0.2s;"
+          @click="handleClick">
     <q-icon color="white"
             name="notifications_active"
             size="48px"/>
-  </div>
+  </button>
 </template>
+
+<script lang="ts"
+        setup>
+
+function handleClick() {
+  const btn = document.querySelector("#event-btn") as HTMLElement;
+
+  if (btn) {
+    btn.style.backgroundColor = "#499613";
+
+    setTimeout(() => {
+      btn.style.backgroundColor = "#060b39";
+    }, 5000);
+  }
+}
+
+</script>
