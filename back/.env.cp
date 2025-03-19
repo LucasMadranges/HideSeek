edbb42f8c2dbf0778026dc9154159d4e083fb5f5
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-03-19 20:40:59 +0100

    🌐 feat(services): enhance socket and backend configurations
    Updated `gameSocket.service.ts` to support CORS, credentials, and specified transport methods. Improved backend with robust CORS settings, environment variable additions, and WebSocketGateway adjustments for tighter domain control.

--- back/.env.cp
+++ back/.env.cp
@@ -2,3 +2,5 @@ DATABASE_URL_USERS=postgresql://admin:password@localhost:5432/hideseek
 POSTGRES_USER=admin
 POSTGRES_PASSWORD=password
 POSTGRES_DB=hideseek
+
+FRONT_URL
--- back/src/main.ts
+++ back/src/main.ts
@@ -1,22 +1,30 @@
-import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+import {NestFactory} from "@nestjs/core";
+import {SwaggerModule, DocumentBuilder} from "@nestjs/swagger";
+import {AppModule} from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  // Configuration Swagger
-  const config = new DocumentBuilder()
-      .setTitle('API Users')
-      .setDescription('Documentation API de gestion des utilisateurs')
-      .setVersion('1.0')
-      .addTag('users')
-      .addBearerAuth() // Si vous utilisez l'authentification JWT
-      .build();
+    // Configuration CORS
+    app.enableCors({
+        origin: ["https://192.168.1.22:9200", "http://localhost:9200"], // Ajoutez tous vos domaines autorisés
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        credentials: true,
+    });
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+    // Configuration Swagger
+    const config = new DocumentBuilder()
+        .setTitle("API Users")
+        .setDescription("Documentation API de gestion des utilisateurs")
+        .setVersion("1.0")
+        .addTag("users")
+        .addBearerAuth() // Si vous utilisez l'authentification JWT
+        .build();
 
-  await app.listen(3000);
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api", app, document);
+
+    await app.listen(3000);
 }
+
 bootstrap();
